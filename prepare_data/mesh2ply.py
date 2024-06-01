9fd7f798d4328c05316faec0c8c97435f71d9885
ARYAN SATPATHY <aryandityan5@gmail.com>
2024-06-01 19:29:45 +0530

    optimization

--- prepare_data/mesh2ply.py
+++ prepare_data/mesh2ply.py
@@ -2,8 +2,21 @@ import os
 import open3d as o3d
 import numpy as np
 
+import multiprocessing as mp
 
-def mesh2pc(num_point,mode,base_data_path):
+def worker(data_path, shape_name, num_point, save_path):
+    ms=o3d.io.read_triangle_mesh(os.path.join(data_path,shape_name))
+    point_cloud=ms.sample_points_poisson_disk(num_point,use_triangle_normal=True)
+    #o3d.visualization.draw_geometries([point_cloud])
+    #print(pc.shape)
+    pc=np.array(point_cloud.points).astype(np.float32)
+    normal=np.array(point_cloud.normals).astype(np.float32)
+    print(shape_name,num_point)
+    #print(pc.shape)
+    #print(normal.shape)
+    np.savetxt(os.path.join(save_path,shape_name[:-4]+'.xyz'),np.concatenate((pc,normal),axis=-1))
+
+def mesh2pc(num_point,mode,base_data_path, pool):
     #num_point=20000
     #mode='train'
     data_path=os.path.join(base_data_path,'%s_mesh'%mode)
@@ -17,24 +30,18 @@ def mesh2pc(num_point,mode,base_data_path):
         pass
 
     for shape_name in shape_name_list:
-
-        ms=o3d.io.read_triangle_mesh(os.path.join(data_path,shape_name))
-        point_cloud=ms.sample_points_poisson_disk(num_point,use_triangle_normal=True)
-        #o3d.visualization.draw_geometries([point_cloud])
-        #print(pc.shape)
-        pc=np.array(point_cloud.points).astype(np.float32)
-        normal=np.array(point_cloud.normals).astype(np.float32)
-        print(shape_name,num_point)
-        #print(pc.shape)
-        #print(normal.shape)
-        np.savetxt(os.path.join(save_path,shape_name[:-4]+'.xyz'),np.concatenate((pc,normal),axis=-1))
-
+        pool.apply_async(worker, (data_path, shape_name, num_point, save_path))
 
 if __name__=='__main__':
-    base_data_path='D:\\PUGEO\\mesh'
+    base_data_path='/home/aryan/baselines/PUGeo/mesh'
     num_point_list=[5000,20000,40000,60000,80000]
     mode_list=['train','test']
     
+    pool = mp.Pool(96)
     for num_point in num_point_list:
+
         for mode in mode_list:
-            mesh2pc(num_point, mode, base_data_path)
+            mesh2pc(num_point, mode, base_data_path, pool)
+
+    pool.close()
+    pool.join()
--- prepare_data/ply2patch.py
+++ prepare_data/ply2patch.py
@@ -3,16 +3,45 @@ import os
 import open3d as o3d
 from pc_util import extract_knn_patch, normalize_point_cloud, farthest_point_sample, group_points,group_points_with_normal
 
+import multiprocessing as mp
 
-
-raw_data_path='D:\\PUGEO\\mesh'
-save_root_path='D:\\PUGEO\\pytorch_data'
-
+raw_data_path='/home/aryan/baselines/PUGeo/mesh'
+save_root_path='/home/aryan/baselines/PUGeo/pytorch_data'
 
 basic_num=5000
 num_point_per_patch=256
-num_patch=int(basic_num/num_point_per_patch*3)
+num_patch=int(basic_num/num_point_per_patch * 3)
 
+def call_worker2(i, mode, up_ratio, input_data_name, centroid, furthest_distance, centroid_points, furthest_distance_patches, centroid_patches):
+    label_data=np.loadtxt(os.path.join(os.path.join(raw_data_path, '%s_%d' % (mode, int(basic_num*up_ratio)),input_data_name)))
+    xyz=(label_data[:,0:3]-centroid)/furthest_distance
+    label_data[:,0:3]=xyz
+    label_patches=group_points_with_normal(label_data,centroid_points,num_point_per_patch*up_ratio)
+    label_patches_xyz=(label_patches[:,:,0:3]-centroid_patches)/furthest_distance_patches
+    label_patches[:,:,0:3]=label_patches_xyz
+    np.save(os.path.join(save_root_path,'%d'%up_ratio,'%d.npy'%i),label_patches)
+    print(input_data_name,up_ratio)
+
+def call_worker(i, up_ratio_list, mode, input_data_name) : 
+    # call([sys.executable, os.path.join(os.path.dirname(__file__), 'beesalgoalter.py'), '--log', '--useNFE'] + args)
+    input_data = np.loadtxt(os.path.join(os.path.join(raw_data_path, '%s_%d' % (mode, int(basic_num)),input_data_name)))
+    xyz,centroid,furthest_distance=normalize_point_cloud(input_data[:,0:3])
+    input_data[:,0:3]=xyz
+    centroid_points=farthest_point_sample(input_data,num_patch)
+    input_patches=group_points_with_normal(input_data,centroid_points,num_point_per_patch)
+    normalized_input_patches_xyz,centroid_patches,furthest_distance_patches=normalize_point_cloud(input_patches[:,:,0:3])
+    input_patches[:,:,0:3]=normalized_input_patches_xyz
+    np.save(os.path.join(save_root_path,'basic','%d.npy'%i),input_patches)
+    
+    # pool = mp.Pool(5)
+
+    results = []
+    for up_ratio in up_ratio_list:
+        results.append(call_worker2(i, mode, up_ratio, input_data_name, centroid, furthest_distance, centroid_points, furthest_distance_patches, centroid_patches))
+
+    # results = [result.get() for result in results]
+    
+    return
 
 def build_dataset(up_ratio_list,mode):
     for up_ratio in up_ratio_list:
@@ -29,25 +58,18 @@ def build_dataset(up_ratio_list,mode):
     input_data_path_list =os.listdir(os.path.join(raw_data_path, '%s_%d' % (mode, int(basic_num))))
     #label_data_path_list =os.listdir( os.path.join(raw_data_path,'%s_%d'%(mode,int(basic_num*up_ratio))))
     i=0
+
+    pool = mp.Pool(60)
+
+    results = []
     for input_data_name in input_data_path_list:
-        input_data=np.loadtxt(os.path.join(os.path.join(raw_data_path, '%s_%d' % (mode, int(basic_num)),input_data_name)))
-        xyz,centroid,furthest_distance=normalize_point_cloud(input_data[:,0:3])
-        input_data[:,0:3]=xyz
-        centroid_points=farthest_point_sample(input_data,num_patch)
-        input_patches=group_points_with_normal(input_data,centroid_points,num_point_per_patch)
-        normalized_input_patches_xyz,centroid_patches,furthest_distance_patches=normalize_point_cloud(input_patches[:,:,0:3])
-        input_patches[:,:,0:3]=normalized_input_patches_xyz
-        np.save(os.path.join(save_root_path,'basic','%d.npy'%i),input_patches)
-        for up_ratio in up_ratio_list:
-            label_data=np.loadtxt(os.path.join(os.path.join(raw_data_path, '%s_%d' % (mode, int(basic_num*up_ratio)),input_data_name)))
-            xyz=(label_data[:,0:3]-centroid)/furthest_distance
-            label_data[:,0:3]=xyz
-            label_patches=group_points_with_normal(label_data,centroid_points,num_point_per_patch*up_ratio)
-            label_patches_xyz=(label_patches[:,:,0:3]-centroid_patches)/furthest_distance_patches
-            label_patches[:,:,0:3]=label_patches_xyz
-            np.save(os.path.join(save_root_path,'%d'%up_ratio,'%d.npy'%i),label_patches)
-            print(input_data_name,up_ratio)
-        i=i+1
+        results.append(pool.apply_async(call_worker, (i, up_ratio_list, mode, input_data_name)))# lambda x : bar.update))
+    
+    pool.close()
+    pool.join()
+
+    results = [result.get() for result in results]
+        
 
 if __name__=='__main__':
     up_ratio_list = [4, 8, 12, 16]
