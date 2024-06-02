80f596ecc5ae08fd3c38161ef8335f3913a15183
ARYAN SATPATHY <aryandityan5@gmail.com>
2024-06-02 16:24:46 +0530

    bug fix

--- prepare_data/ply2patch.py
+++ prepare_data/ply2patch.py
@@ -19,7 +19,8 @@ def call_worker2(i, mode, up_ratio, input_data_name, centroid, furthest_distance
     label_patches=group_points_with_normal(label_data,centroid_points,num_point_per_patch*up_ratio)
     label_patches_xyz=(label_patches[:,:,0:3]-centroid_patches)/furthest_distance_patches
     label_patches[:,:,0:3]=label_patches_xyz
-    np.save(os.path.join(save_root_path,'%d'%up_ratio,'%d.npy'%i),label_patches)
+    np.save(os.path.join(save_root_path, mode,'%d'%up_ratio,'%d.npy'%i),label_patches)
+
     print(input_data_name,up_ratio)
 
 def call_worker(i, up_ratio_list, mode, input_data_name) : 
@@ -31,7 +32,9 @@ def call_worker(i, up_ratio_list, mode, input_data_name) :
     input_patches=group_points_with_normal(input_data,centroid_points,num_point_per_patch)
     normalized_input_patches_xyz,centroid_patches,furthest_distance_patches=normalize_point_cloud(input_patches[:,:,0:3])
     input_patches[:,:,0:3]=normalized_input_patches_xyz
-    np.save(os.path.join(save_root_path,'basic','%d.npy'%i),input_patches)
+    np.save(os.path.join(save_root_path, mode, 'basic','%d.npy'%i),input_patches)
+    np.save(os.path.join(save_root_path, mode,'basic','%d_centroid.npy'%i),centroid_patches)
+    np.save(os.path.join(save_root_path, mode,'basic','%d_fd.npy'%i),furthest_distance_patches)
     
     # pool = mp.Pool(5)
 
@@ -45,24 +48,17 @@ def call_worker(i, up_ratio_list, mode, input_data_name) :
 
 def build_dataset(up_ratio_list,mode):
     for up_ratio in up_ratio_list:
-        try:
-            os.mkdir(os.path.join(save_root_path,'%d'%up_ratio))
-
-        except:
-            pass
-    try:
-        os.mkdir(os.path.join(save_root_path, 'basic'))
-    except:
-        pass
-
-    input_data_path_list =os.listdir(os.path.join(raw_data_path, '%s_%d' % (mode, int(basic_num))))
+        os.makedirs(os.path.join(save_root_path, mode,'%d'%up_ratio), exist_ok = True)
+        
+    os.makedirs(os.path.join(save_root_path, mode, 'basic'), exist_ok = True)
+    
+    input_data_path_list = os.listdir(os.path.join(raw_data_path, '%s_%d' % (mode, int(basic_num))))
     #label_data_path_list =os.listdir( os.path.join(raw_data_path,'%s_%d'%(mode,int(basic_num*up_ratio))))
-    i=0
 
     pool = mp.Pool(60)
 
     results = []
-    for input_data_name in input_data_path_list:
+    for (i, input_data_name) in enumerate(input_data_path_list):
         results.append(pool.apply_async(call_worker, (i, up_ratio_list, mode, input_data_name)))# lambda x : bar.update))
     
     pool.close()
@@ -74,3 +70,4 @@ def build_dataset(up_ratio_list,mode):
 if __name__=='__main__':
     up_ratio_list = [4, 8, 12, 16]
     build_dataset(up_ratio_list,'train')
+    build_dataset(up_ratio_list,'test')
